144953dc10ac05bb7d3be5290bed38eccb0f740d
Pranay4053 <saipranays97@gmail.com>
2021-11-13 13:52:50 -0600

    updated code

--- routes/fruit.js
+++ routes/fruit.js
@@ -1,7 +1,18 @@
-var express = require('express'); 
-const fruit_controlers= require('../controllers/fruit'); 
-var router = express.Router(); 
- 
-/* GET fruits */ 
-router.get('/', fruit_controlers.fruit_view_all_Page ); 
-module.exports = router; 
+var express = require('express');
+const controlers = require('../controllers/fruit');
+var router = express.Router();
+
+/* GET costumes */
+router.get('/', controlers.fruit_view_all_Page);
+
+router.get('/detail', controlers.fruit_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', controlers.fruit_create_Page);
+
+
+router.get('/update', controlers.fruit_update_Page); 
+
+router.get('/delete', controlers.fruit_delete_Page); 
+
+module.exports = router;
